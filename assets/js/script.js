2555b1e1c18566b404dea587c233a71deaf0a347
nshaw973 <nshaw973@gmail.com>
2023-02-09 14:41:20 -0800

    Cleaned up the first weather deatails that will show up for the current date. added the icon that will reflect the current weather conditions as well.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
 
 var searchBarEl = document.querySelector('#search')
-
+var currentCity = document.querySelector('#city-name')
+var cityState = document.querySelector('#city-state');
+var weatherIcon = document.querySelector('#icon');
+var temp = document.querySelector('#temp');
+var wind = document.querySelector('#wind');
+var humidity = document.querySelector('#humidity');
 
 //City Searcher ------------------------------------------------------------------//
  
@@ -34,8 +39,9 @@ function getCity(requestUrl) {
 
     }).then(function (data) {
 
-        console.log(data)
+        console.log(data);
 
+        //This will be used to get the coordinates needed for the weatherapi fetch
         cityData = {
           city: data[0].name,
           lon: data[0].lon,
@@ -43,19 +49,48 @@ function getCity(requestUrl) {
           state: data[0].state,
         }
 
-        cityWeather(cityData)
+        cityWeather(cityData);
 
     });
   }
 
   function cityWeather (cityData) {
+ 
+    console.log('current city = ' + cityData.city);
+    console.log("lat = " + cityData.lat + "lon = " + cityData.lon);
 
-    var currentCity = document.querySelector('#city-name');
+    //added impaerial units to the link, as well as a total count of 6 instead of the default 40//
+    var cityWeatherData = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + cityData.lat + '&lon=' + cityData.lon + '&units=imperial&cnt=6&appid=6dccc6347181beac7daedbd684110b84'
 
-    currentCity.textContent = cityData.city
+    fetch(cityWeatherData)
+      .then(function (response) {
+        console.log(response);
+
+        return response.json();
+
+    }).then(function (data) {
+      console.log(data);
+
+      var cityWeatherDetails = {
+        temp: data.list[0].main.temp,
+        wind: data.list[0].wind.speed,
+        humidity: data.list[0].main.humidity,
+        icon: data.list[0].weather[0].icon,
+        id: data.list[0].weather[0].id
+      }
     
-    console.log('current city = ' + cityData.city)
-    console.log("lat = " + cityData.lat + "lon = " + cityData.lon)
+    var icon = 'http://openweathermap.org/img/wn/' + cityWeatherDetails.icon + '@2x.png'
 
+      console.log(icon)
+
+      currentCity.textContent = cityData.city;
+      cityState.textContent = cityData.state;
+      weatherIcon.setAttribute('src', icon)
+
+      temp.textContent = cityWeatherDetails.temp;
+      wind.textContent = cityWeatherDetails.wind;
+      humidity.textContent = cityWeatherDetails.humidity;
+
+    }) 
   }
 
